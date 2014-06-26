3d46e9dcc7cc7973d3d5806892752d4eaf5139a0
Yuri Vishnevsky <yurivish@gmail.com>
2014-06-26 18:39:58 -0500

    Add the pixels and ui code.

---
+++ pixels.jl
@@ -0,0 +1,321 @@
+module Pixels
+
+using Images
+
+include("ui.jl")
+include("bag.jl")
+include("treap.jl")
+include("sss.jl")
+
+# 24 bits for color, 24 bits for xy (4096 each), 
+
+immutable NeighborStats
+    n::Int
+    avg::Rgb{Uint8}
+    sumvar::Uint
+    sqrt_sumvar::Uint8
+    NeighborStats(n, avg, sumvar) = new(n, avg, sumvar, uint8(sqrt(sumvar)))
+    NeighborStats() = new(0, Rgb{Uint8}(), 0, 0)
+end
+
+# NOTE: We could stuff x, y, filled, bagindex into an int, and use an iterator for neighbors.
+type Vertex
+    x::Int
+    y::Int
+    color::GLPixel
+    filled::Bool
+    nominated::Bool
+    bagindex::Int
+    stats::NeighborStats
+    neighbors::Vector{Vertex}
+    t::Float64 # Used to preserve key uniqueness and break ties in <(Vertex, Vertex)
+    function Vertex(x, y)
+        neighbors = Vertex[]
+        sizehint(neighbors, 8)
+        new(x, y, GLPixel(), false, false, 0, NeighborStats(), neighbors, rand(Float64))
+    end
+end
+
+Base.start(p::Rgb) = 1
+Base.next{T}(p::Rgb{T}, state::Int) =
+    state == 1 ? (p.r, 2) :
+    state == 2 ? (p.g, 3) :
+    state == 3 ? (p.b, 4) : throw("Iteration state error.")
+Base.done(p::Rgb, state::Int) = state == 4
+
+type Canvas
+    grid::Array{Vertex}
+    frontier
+    function Canvas(width, height, frontier)
+        grid = [Vertex(x, y) for y in 1:height, x in 1:width]
+ 
+        for x in 1:width, y in 1:height
+            for nx in max(x - 1, 1):min(x + 1, width), ny in max(y - 1, 1):min(y + 1, height)
+                !(nx == x && ny == y) && push!(grid[y, x].neighbors, grid[ny, nx])
+            end
+        end
+        new(grid, frontier)
+    end
+end
+frontier(canvas) = canvas.frontier
+Base.getindex(c::Canvas, a, b) = c.grid[a, b]
+neighbors(vertex::Vertex) = vertex.neighbors
+isnominated(vertex::Vertex) = vertex.bagindex != 0 || vertex.nominated # TODO: Fix.
+
+function nominate!(vertex, frontier::Bag{Vertex})
+    @assert !isnominated(vertex)
+
+    # Bag add
+    push!(frontier, vertex)
+    vertex.bagindex = length(frontier)
+end
+
+function withdraw!(vertex, frontier::Bag{Vertex})
+    @assert isnominated(vertex)
+
+    # Bag remove
+    index = vertex.bagindex
+    deleteat!(frontier, index)
+    length(frontier) >= index && (frontier[index].bagindex = index)
+    vertex.bagindex = 0
+end
+
+function nominate!(vertex, frontier::Treap)
+    @assert !vertex.nominated
+    add!(frontier, vertex)
+    vertex.nominated = true
+end
+
+function withdraw!(vertex, frontier::Treap)
+    @assert vertex.nominated
+    remove!(frontier, vertex)
+    vertex.nominated = false
+end
+
+hasemptyneighbors(vertex) = vertex.stats.n < length(neighbors(vertex))
+emptyneighbors(vertex) = filter((v) -> !v.filled, neighbors(vertex))
+function randemptyneighbor(vertex)
+    prob = 1.0
+    local neighbor
+    for v in neighbors(vertex)
+        if !v.filled
+            if rand() < prob
+                neighbor = v
+            end
+            prob /= 2
+        end
+    end
+    neighbor
+end
+
+function fill!(pixels, frontier, vertex, color, outer::Bool)
+    # TODO: Don't pass pixels in.
+    pixels[vertex.y, vertex.x] = color
+
+    vertex.color = color
+    vertex.filled = true
+    if outer
+        # Outer frontier
+        for v in neighbors(vertex)
+            # Withdraw in order to re-insert neighbors already on the frontier,
+            # as their stats have changed in a way that may influence their
+            # storage in the frontier
+            isnominated(v) && withdraw!(v, frontier)
+            stats!(v)
+            !isnominated(v) && !v.filled && nominate!(v, frontier)
+        end
+        isnominated(vertex) && withdraw!(vertex, frontier)
+    else
+        # Inner frontier
+        for v in neighbors(vertex)
+            # NOTE: Does something like the re-insertion above need to happen here as well?
+            stats!(v)
+            isnominated(v) && !hasemptyneighbors(v) && withdraw!(v, frontier)
+        end
+        hasemptyneighbors(vertex) && nominate!(vertex, frontier)
+    end
+end
+
+# Calculate average without overflow: (R - L)/2 + L
+function stats!(vertex)
+    n = zero(vertex.stats.n)
+    avg = var = Rgb{Float64}() #zero(vertex.stats.avg)
+
+    for v in neighbors(vertex)
+        if v.filled
+            n += 1
+            avg += v.color
+            # Promote types before squaring, since Rgb{Uint8} * Rgb{Uint8} => Rgb{Uint8}
+            var += oftype(var, v.color) * v.color
+        end
+    end
+
+    if n == 0
+        vertex.stats = NeighborStats()
+    else
+        avg = avg / n
+        var = var / n - avg * avg
+        vertex.stats = NeighborStats(n, Rgb{Uint8}(uint8(avg.r), uint8(avg.g), uint8(avg.b)), uint(sum(var)))
+    end
+end
+
+r() = rand() - 0.5
+mutate(color::GLPixel, scale) = GLPixel(
+    clamp(color.r + int(scale * r()), typemin(color.r), typemax(color.r)),
+    clamp(color.g + int(scale * r()), typemin(color.g), typemax(color.g)),
+    clamp(color.b + int(scale * r()), typemin(color.b), typemax(color.b))
+)
+
+function nearest(frontier::Bag{Vertex}, color)
+    best = frontier[1]
+    best_dist::Float64 = Inf
+    for v in frontier
+        d = sqdiff(v.stats.avg, color) + v.stats.sumvar
+        if d < best_dist
+            best = v
+            best_dist = d
+        end
+    end
+    best
+end
+
+function loadimage(imgpath::String)
+    im = convert(Array, imread(imgpath))
+    w, h = size_spatial(im)
+    colors = Pixel[]
+    for i in 1:w, j in 1:h
+        push!(colors, Pixel(im[i, j, 1], im[i, j, 2], im[i, j, 3]))
+    end
+    colors
+end
+
+SPEED = 1000
+
+function evolve(pixels)
+    (height, width) = size(pixels)
+    canvas = Canvas(width, height, Bag{Vertex}(height * width))
+
+    fill!(pixels, frontier(canvas), canvas[div(height, 2), div(width, 2)], GLPixel(255, 255, 255), true)
+    for i in 1:length(canvas.grid) - 1
+        vertex = frontier(canvas)[rand(1:length(frontier(canvas)))]
+        color = filter(n -> n.filled, neighbors(vertex))[1].color
+        fill!(pixels, frontier(canvas), vertex, mutate(color, 15), true)
+        i % SPEED == 0 && produce(pixels)
+    end
+    pixels
+end
+
+function gencolors(imgpath::String)
+    colors = shuffle!(loadimage(imgpath))
+    sort!(colors, alg=QuickSort, lt=(p::Pixel, q::Pixel) -> hue(p) < hue(q))
+    colors
+end
+
+function gencolors(n::Int)
+    colors = [rand(GLPixel) for i in 1:n]
+    # sort!(colors, alg=QuickSort, lt=(p::Pixel, q::Pixel) -> hue(p) < hue(q))
+    sort!(colors, rev=true, alg=QuickSort, lt=(p::Pixel, q::Pixel) -> p.r < q.r && p.g < q.g && p.b < q.b)
+    # sort!(colors, rev=true, alg=QuickSort, by=(p::Pixel) -> p.b + p.g^2)
+    colors
+end
+
+function placeavg(pixels)
+    (height, width) = size(pixels)
+    canvas = Canvas(width, height, Treap{Vertex}())
+    # colors = gencolors(length(pixels))
+    colors = gencolors("images/Bachalpseeflowers.jpg")
+    # colors = gencolors(1000)
+    # colors = gencolors("Bachalpseeflowers.jpg")
+    i = 1
+    fill!(pixels, frontier(canvas), canvas[div(height, 2), div(width, 2)], colors[i += 1], true)
+    while i < length(pixels)
+        color = colors[mod1(i, length(colors))]
+        vertex = nearest(frontier(canvas), color)
+        fill!(pixels, frontier(canvas), vertex, color, true)
+        i % SPEED == 0 && produce(pixels)
+        i += 1
+    end
+    pixels
+end
+
+
+function placemin(pixels)
+    (height, width) = size(pixels)
+    canvas = Canvas(width, height, Treap{Vertex}())
+    # colors = gencolors(length(pixels))
+    # colors = gencolors("Bachalpseeflowers.jpg")
+    colors = gencolors("images/Bachalpseeflowers.jpg")
+    i = 1
+
+    fill!(pixels, frontier(canvas), canvas[div(height, 2), div(width, 2)], colors[i += 1], false)
+    # fill!(pixels, frontier(canvas), canvas[200, 200], colors[i += 1], false)
+    # fill!(pixels, frontier(canvas), canvas[1, 1], colors[i += 1], false)
+
+    while i < length(pixels)
+        color = colors[mod1(i, length(colors))]
+        candidate = nearest(frontier(canvas), color)
+        vertex = randemptyneighbor(candidate)
+        fill!(pixels, frontier(canvas), vertex, color, false)
+        i % SPEED == 0 && produce(pixels)
+        i += 1
+    end
+    pixels
+end
+
+# :min / :avg / :evo
+fntype = :min
+
+fn = fntype == :min ? placemin :
+     fntype == :avg ? placeavg :
+     fntype == :evo ? evolve : throw("fntype must be :min, :avg, or :evo.")
+
+if fntype == :min || fntype == :evo
+    # Minimum selection
+    Base.size(p::Vertex, n) = n == 1 ? 3 : error("Vertices are one-dimensional.")
+    Base.getindex(p::Vertex, n) = p.color[n]
+    <(p::Vertex, q::Vertex) = p.color == q.color ? p.t < q.t : shuffless(p.color, q.color)
+    <(p::Vertex, q::Rgb) = shuffless(p.color, q)
+    <(p::Rgb, q::Vertex) = shuffless(p, q.color)
+    dist_sq{T}(p::Rgb{T}, q::Rgb{T}) = sqdiff(p, q)
+    dist_sq{T}(p::Vertex, q::Rgb{T}) = sqdiff(p.color, q)
+    dist_sq{T}(p::Rgb{T}, q::Vertex) = sqdiff(p, q.color)
+else
+    # Average selection
+    Base.size(p::Vertex, n) = n == 1 ? 4 : error("n must be 1.")
+    Base.getindex(p::Vertex, n) = n < 4 ? uint8(p.stats.avg[n]) : n == 4 ? uint8(p.stats.sqrt_sumvar) : error("Index out of bounds: $n")
+    # If the colors are equal, compare t values; we need this because the treap enforces uniqueness.
+    <(p::Vertex, q::Vertex) = shuffless(p, q) ? true : shuffmore(p, q) ? false : p.t < q.t
+    <(p::Vertex, q::Rgb) = shuffless(p, q)
+    <(p::Rgb, q::Vertex) = shuffless(p, q)
+    dist_sq{T}(p::Rgb{T}, q::Rgb{T}) = sqdiff(p, q)
+    dist_sq{T}(p::Vertex, q::Rgb{T}) = sqdiff(p.stats.avg, q) + p.stats.sumvar
+    dist_sq{T}(p::Rgb{T}, q::Vertex) = sqdiff(p, q.stats.avg) + q.stats.sumvar
+
+    # Avg color: Hacked version with p[4] == 0
+    Base.start(p::Rgb) = 1
+    Base.next{T}(p::Rgb{T}, state::Int) =
+        state == 1 ? (p.r, 2) :
+        state == 2 ? (p.g, 3) :
+        state == 3 ? (p.b, 4) :
+        state == 4 ? (zero(T), 5) : throw("Iteration state error.")
+    Base.done(p::Rgb, state::Int) = state == 5
+    Base.getindex{T}(p::Rgb{T}, n::Int) = n == 1 ? p.r : n == 2 ? p.g : n == 3 ? p.b : n == 4 ? zero(T) : throw("Color indexing error.")
+    Base.size(p::Rgb) = (4,)
+    Base.size(p::Rgb, n) = n == 1 ? 4 : throw("Invalid dimension.")
+end
+
+# placemin(zeros(GLPixel, 1440, 900))
+# # @time placemin(zeros(GLPixel, 200, 200))
+# @time placemin(zeros(GLPixel, 100, 100))
+# # @time placemin(zeros(GLPixel, 125, 125))
+# @time placemin(zeros(GLPixel, 150, 150))
+# # @time placemin(zeros(GLPixel, 175, 175))
+# @time placemin(zeros(GLPixel, 200, 200))
+# @time placemin(zeros(GLPixel, 500, 500))
+
+# display(2560, 1440 - 22, fn; title="Pixels")
+# display(1440, 900 - 22, fn; title="Pixels")
+display(1000, 750, fn; title="Pixels")
+# display(800, 538, fn; title="Pixels")
+
+end
---
+++ ui.jl
@@ -0,0 +1,190 @@
+import GLFW
+using ModernGL
+
+include("gltools.jl")
+include("rgb.jl")
+
+type MouseState
+    x::Float64
+    y::Float64
+    pressed::Bool
+end
+
+function pixelgen(pixels)
+    for i in 1:length(pixels)
+        pixels[i] = GLPixel(i, i * 3, i * 7)
+        i % 1000 == 0 && produce(pixels)
+    end
+    pixels
+end
+
+function display(width, height, f; title="Julia")
+    GLFW.Init()
+     
+    @osx_only begin
+        # OS X-specific GLFW hints to specify the correct 
+        # version of OpenGL at context creation time
+        GLFW.WindowHint(GLFW.CONTEXT_VERSION_MAJOR, 3)
+        GLFW.WindowHint(GLFW.CONTEXT_VERSION_MINOR, 2)
+        GLFW.WindowHint(GLFW.OPENGL_PROFILE, GLFW.OPENGL_CORE_PROFILE)
+        GLFW.WindowHint(GLFW.OPENGL_FORWARD_COMPAT, GL_TRUE)
+    end
+
+    retinascale = 2
+    rwidth, rheight = width, height
+    # rwidth *= retinascale
+    # rheight *= retinascale
+    # width = div(width, retinascale)
+    # height = div(height, retinascale)
+    
+    # Create a window with an OpenGL context
+    window = GLFW.CreateWindow(width, height, title)
+     
+    # Make the window's context current
+    GLFW.MakeContextCurrent(window)
+     
+    # Initialize a vertex array
+    vao = glGenVertexArray()
+    glBindVertexArray(vao)
+    glCheckError("binding vertex array")
+
+    # Initialize a buffer for vertices
+    vbo = glGenBuffer()
+    glBindBuffer(GL_ARRAY_BUFFER, vbo)
+    glCheckError("binding buffer")
+
+    # Bind the data to the VBO
+    # Specifies a fullscreen rectangle in standard coordinates.
+    # [top left, top right, bottom left, bottom right]
+    data = GLfloat[1, -1, 1, 1, -1, -1, -1, 1]
+    glBufferData(GL_ARRAY_BUFFER, sizeof(data), data, GL_STATIC_DRAW)
+    glCheckError("setting buffer data")
+
+    # Initialize a texture for color data
+    tex = glGenTexture()
+    glBindTexture(GL_TEXTURE_2D, tex)
+    glTexStorage2D(GL_TEXTURE_2D, 1, GL_RGB8, width, height)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
+
+    glCheckError("initializing texture")
+
+    # Create and initialize shaders
+    program = begin
+        const vsh = """
+            #version 330
+            in vec2 position;
+         
+            void main() {
+                gl_Position = vec4(position, 0.0, 1.0);
+            }
+        """
+         
+        const fsh = """
+            #version 330
+            out vec4 outColor;
+            uniform vec2 screenDims;
+            uniform sampler2D tex;
+            uniform float zoom;
+            
+            void main() {
+                vec2 pos = gl_FragCoord.xy / screenDims;
+                $(
+                    if retinascale > 1.0
+                        "vec2 zeroToCenter = vec2(0.0, 0.0);" # A hack for retina screens
+                    else
+                        "vec2 zeroToCenter = vec2(0.5, 0.5);"
+                    end
+                )
+                outColor = texture(tex, (pos - zeroToCenter) * zoom + zeroToCenter);
+            }
+        """
+
+        vertexShader = createShader(vsh, GL_VERTEX_SHADER)
+        fragmentShader = createShader(fsh, GL_FRAGMENT_SHADER)
+        program = createShaderProgram(vertexShader, fragmentShader) do prog
+            glBindFragDataLocation(prog, 0, "outColor")
+        end
+
+        glUseProgram(program)
+
+        # TODO: How do we ensure these values are GLfloats?
+        screenDimsUniform = glGetUniformLocation(program, "screenDims")
+        @assert screenDimsUniform > -1
+        glUniform2f(screenDimsUniform, float(width), float(height))
+
+        textureUniform = glGetUniformLocation(program, "tex")
+        @assert textureUniform > -1
+
+        zoomUniform = glGetUniformLocation(program, "zoom")
+        @assert zoomUniform > -1
+        glUniform1f(zoomUniform, 1.0)
+
+        positionAttribute = glGetAttribLocation(program, "position")
+        @assert positionAttribute > -1
+        glEnableVertexAttribArray(positionAttribute)
+        glVertexAttribPointer(positionAttribute, 2, GL_FLOAT, false, 0, 0)
+
+        program
+    end
+
+    function drawtexture()
+        # Activate and bind the texture
+        glActiveTexture(GL_TEXTURE0)
+        glBindTexture(GL_TEXTURE_2D, tex)
+
+        # The uniform value for a sampler refers to the texture unit (0 refers to GL_TEXTURE0)
+        glUniform1i(textureUniform, 0)
+
+        # Draw the textured rectangle
+        glBindVertexArray(vao)
+        glBindBuffer(GL_ARRAY_BUFFER, vbo)
+        glDrawArrays(GL_TRIANGLE_STRIP, 0, 4)
+    end
+
+    global generator
+    regenerate(f, args...) = global generator = @task f(zeros(GLPixel, width, height), args...)
+    regenerate(f)
+
+    GLFW.SetKeyCallback(window, (key, scancode, action, mods) -> begin
+        if action == GLFW.RELEASE
+            key = uppercase(convert(Char, key))
+            key == 'R' && regenerate(f)
+            key == 'Q' && GLFW.SetWindowShouldClose(window, GL_TRUE)
+        end
+    end)
+
+    gc_disable()
+
+    # Event loop
+    zoom::GLfloat = 1.0 / retinascale
+    while !GLFW.WindowShouldClose(window)   
+        # Clear the screen
+        glClearColor(0.0, 0.0, 0.0, 1.0)
+        glClear(GL_COLOR_BUFFER_BIT)
+
+        if !istaskdone(generator)
+            (mx, my) = GLFW.GetCursorPos(window)
+            mx = clamp(mx, 0, width) / width
+            my = clamp(my, 0, height) / height
+            pressed = GLFW.GetMouseButton(window, GLFW.MOUSE_BUTTON_LEFT) == GLFW.PRESS
+            glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, width, height, GL_RGB, GL_UNSIGNED_BYTE, consume(generator, MouseState(mx, my, pressed)))
+        end
+
+        glUniform1f(zoomUniform, zoom)
+        drawtexture()
+
+        # Swap front and back buffers
+        GLFW.SwapBuffers(window)
+     
+        # Poll for and process events
+        GLFW.PollEvents()
+    end
+    gc_enable()
+    gc()
+
+    glDeleteTextures(1, [tex])
+    GLFW.Terminate()
+end
+
+ 
